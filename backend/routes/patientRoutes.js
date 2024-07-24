29bd592d8d6a3d788f88c8b5d98943a5c77edc5b
sitijulianapakaya <sitijulianapakaya@gmail.com>
2024-07-24 18:01:28 +0800

    Update patientRoutes.js

--- backend/routes/patientRoutes.js
+++ backend/routes/patientRoutes.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const patientController = require('../controllers/patientController');
+
+router.get('/', patientController.getPatients);
+
+// Tambahkan route lainnya untuk CRUD
+
+module.exports = router;
 
