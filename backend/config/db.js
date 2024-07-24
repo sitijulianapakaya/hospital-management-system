7cb4846c4cd315f436431d5a1436ea12dcef9c17
sitijulianapakaya <sitijulianapakaya@gmail.com>
2024-07-24 17:58:18 +0800

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,19 @@
+const mongoose = require('mongoose');
+const keys = require('./keys');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(keys.mongoURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+        });
+        console.log('MongoDB connected...');
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
 
