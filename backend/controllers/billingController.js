1b8eb6cf0514d98e1f2229b52166de473e9081ca
sitijulianapakaya <sitijulianapakaya@gmail.com>
2024-07-24 18:13:43 +0800

    Update billingController.js

--- backend/controllers/billingController.js
+++ backend/controllers/billingController.js
@@ -1,1 +1,13 @@
+const Billing = require('../models/Billing');
+
+exports.getBillings = async (req, res) => {
+    try {
+        const billings = await Billing.find();
+        res.json(billings);
+    } catch (err) {
+        res.status(500).send('Server Error');
+    }
+};
+
+// Tambahkan fungsi lainnya untuk CRUD
 
