4a3f4e277a965949fcdb09947e7d392aa875ef1c
sitijulianapakaya <sitijulianapakaya@gmail.com>
2024-07-24 18:22:27 +0800

    Update appointmentRoutes.js

--- backend/routes/appointmentRoutes.js
+++ backend/routes/appointmentRoutes.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const appointmentController = require('../controllers/appointmentController');
+
+router.get('/', appointmentController.getAppointments);
+
+// Tambahkan route lainnya untuk CRUD
+
+module.exports = router;
 
