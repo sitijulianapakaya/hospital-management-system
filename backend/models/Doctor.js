4b72e535203fceb9b9a971494f378e5565bf198b
sitijulianapakaya <sitijulianapakaya@gmail.com>
2024-07-24 18:14:41 +0800

    Update Doctor.js

--- backend/models/Doctor.js
+++ backend/models/Doctor.js
@@ -1,1 +1,16 @@
+const mongoose = require('mongoose');
+
+const DoctorSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    specialization: {
+        type: String,
+        required: true,
+    },
+    // Tambahkan field lain sesuai kebutuhan
+});
+
+module.exports = Doctor = mongoose.model('doctor', DoctorSchema);
 
