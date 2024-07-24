e1f9e9c9263795999b6053477cba0052be499a67
sitijulianapakaya <sitijulianapakaya@gmail.com>
2024-07-24 18:12:36 +0800

    Update doctorController.js

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -1,1 +1,13 @@
+const Doctor = require('../models/Doctor');
+
+exports.getDoctors = async (req, res) => {
+    try {
+        const doctors = await Doctor.find();
+        res.json(doctors);
+    } catch (err) {
+        res.status(500).send('Server Error');
+    }
+};
+
+// Tambahkan fungsi lainnya untuk CRUD
 
