8dcf13b8f914a3ca04faf0cec0d207d318ef88ac
sitijulianapakaya <sitijulianapakaya@gmail.com>
2024-07-24 18:15:28 +0800

    Update Appointment.js

--- backend/models/Appointment.js
+++ backend/models/Appointment.js
@@ -1,1 +1,22 @@
+const mongoose = require('mongoose');
+
+const AppointmentSchema = new mongoose.Schema({
+    patient: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'patient',
+        required: true,
+    },
+    doctor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'doctor',
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    // Tambahkan field lain sesuai kebutuhan
+});
+
+module.exports = Appointment = mongoose.model('appointment', AppointmentSchema);
 
