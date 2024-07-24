7b2b391e743f8239a54c73c2507573da381f5890
sitijulianapakaya <sitijulianapakaya@gmail.com>
2024-07-24 18:13:22 +0800

    Update medicationController.js

--- backend/controllers/medicationController.js
+++ backend/controllers/medicationController.js
@@ -1,1 +1,13 @@
+const Medication = require('../models/Medication');
+
+exports.getMedications = async (req, res) => {
+    try {
+        const medications = await Medication.find();
+        res.json(medications);
+    } catch (err) {
+        res.status(500).send('Server Error');
+    }
+};
+
+// Tambahkan fungsi lainnya untuk CRUD
 
