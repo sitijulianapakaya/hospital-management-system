ba19936064d917b21256beed65bb118878a8819d
sitijulianapakaya <sitijulianapakaya@gmail.com>
2024-07-24 18:21:39 +0800

    Update billingRoutes.js

--- backend/routes/billingRoutes.js
+++ backend/routes/billingRoutes.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const billingController = require('../controllers/billingController');
+
+router.get('/', billingController.getBillings);
+
+// Tambahkan route lainnya untuk CRUD
+
+module.exports = router;
 
