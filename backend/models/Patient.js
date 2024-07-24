1ac76b2d80f5b1fa12da2e649abc72ad8a3a56b1
sitijulianapakaya <sitijulianapakaya@gmail.com>
2024-07-24 18:00:58 +0800

    Update Patient.js

--- backend/models/Patient.js
+++ backend/models/Patient.js
@@ -1,1 +1,20 @@
+const mongoose = require('mongoose');
+
+const PatientSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    age: {
+        type: Number,
+        required: true,
+    },
+    gender: {
+        type: String,
+        required: true,
+    },
+    // Tambahkan field lain sesuai kebutuhan
+});
+
+module.exports = Patient = mongoose.model('patient', PatientSchema);
 
