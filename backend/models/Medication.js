c648a43e9529ab8c340d3f408d71171abdb2ec8f
sitijulianapakaya <sitijulianapakaya@gmail.com>
2024-07-24 18:15:50 +0800

    Update Medication.js

--- backend/models/Medication.js
+++ backend/models/Medication.js
@@ -1,1 +1,16 @@
+const mongoose = require('mongoose');
+
+const MedicationSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    dosage: {
+        type: String,
+        required: true,
+    },
+    // Tambahkan field lain sesuai kebutuhan
+});
+
+module.exports = Medication = mongoose.model('medication', MedicationSchema);
 
