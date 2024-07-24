6c5d5b8d5c21c68520188784f7922dc769d5ff80
sitijulianapakaya <sitijulianapakaya@gmail.com>
2024-07-24 18:16:32 +0800

    Update Billing.js

--- backend/models/Billing.js
+++ backend/models/Billing.js
@@ -1,1 +1,21 @@
+const mongoose = require('mongoose');
+
+const BillingSchema = new mongoose.Schema({
+    patient: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'patient',
+        required: true,
+    },
+    amount: {
+        type: Number,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    // Tambahkan field lain sesuai kebutuhan
+});
+
+module.exports = Billing = mongoose.model('billing', BillingSchema);
 
