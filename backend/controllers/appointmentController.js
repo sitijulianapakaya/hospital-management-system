c8e9ed247284359b27625a728658baab06a5f9be
sitijulianapakaya <sitijulianapakaya@gmail.com>
2024-07-24 18:13:00 +0800

    Update appointmentController.js

--- backend/controllers/appointmentController.js
+++ backend/controllers/appointmentController.js
@@ -1,1 +1,13 @@
+const Appointment = require('../models/Appointment');
+
+exports.getAppointments = async (req, res) => {
+    try {
+        const appointments = await Appointment.find();
+        res.json(appointments);
+    } catch (err) {
+        res.status(500).send('Server Error');
+    }
+};
+
+// Tambahkan fungsi lainnya untuk CRUD
 
