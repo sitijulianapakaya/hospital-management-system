54f5fcfa5acc0e945fe339450a61b4bb1d975bef
sitijulianapakaya <sitijulianapakaya@gmail.com>
2024-07-24 18:04:16 +0800

    Update server.js

--- backend/utlis/server.js
+++ backend/utlis/server.js
@@ -1,1 +1,18 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+// Define Routes
+app.use('/api/patients', require('./routes/patientRoutes'));
+// Tambahkan route lainnya sesuai kebutuhan
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
