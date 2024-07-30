a1f9e40d8d2947dcdd636ad85146ff359448993c
sitijulianapakaya <sitijulianapakaya@gmail.com>
2024-07-30 17:28:37 +0800

    Update appointmentReducer.js

--- backend/reducers/appointmentReducer.js
+++ backend/reducers/appointmentReducer.js
@@ -1,1 +1,12 @@
+// store.js
+import { createStore, combineReducers } from 'redux';
+import appointmentReducer from './appointmentReducer';
+
+const rootReducer = combineReducers({
+  appointments: appointmentReducer,
+});
+
+const store = createStore(rootReducer);
+
+export default store;
 
