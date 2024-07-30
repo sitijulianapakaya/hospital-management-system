728193f7c228e6b2c3f82b761f94e4f2a53c3add
sitijulianapakaya <sitijulianapakaya@gmail.com>
2024-07-30 19:06:13 +0800

    Update index.js

--- backend/services/index.js
+++ backend/services/index.js
@@ -1,1 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './store'; // Import Redux store
+import App from './App'; // Import komponen utama aplikasi
+import './index.css'; // Import file CSS global
 
