e65ac6f944f21a2a0c04cfc4f3a78d9e2dd99c60
sitijulianapakaya <sitijulianapakaya@gmail.com>
2024-07-30 19:37:06 +0800

    Update Navbar.js

--- backend/frontend/public/src/assets/images/components/Navbar.js
+++ backend/frontend/public/src/assets/images/components/Navbar.js
@@ -1,1 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><Link to="/doctors">Doctors</Link></li>
+        <li><Link to="/medications">Medications</Link></li>
+        <li><Link to="/patients">Patients</Link></li>
+        <li><Link to="/appointments">Appointments</Link></li>
+        <li><Link to="/bills">Billing</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
 
