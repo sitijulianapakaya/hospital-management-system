89d86013e253744502f594413d3a8ee2da9dbaee
sitijulianapakaya <sitijulianapakaya@gmail.com>
2024-07-30 17:16:40 +0800

    Update NotFoundPage.js

--- backend/pages/NotFoundPage.js
+++ backend/pages/NotFoundPage.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+      <Link to="/">Go back to Home</Link>
+    </div>
+  );
+};
+
+export default NotFoundPage;
 
