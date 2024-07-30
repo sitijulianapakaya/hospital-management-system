399373331f8f1db528a7d8d6acbb52f5c5db8a4d
sitijulianapakaya <sitijulianapakaya@gmail.com>
2024-07-30 19:36:46 +0800

    Update MedicationList.js

--- backend/frontend/public/src/assets/images/components/MedicationList.js
+++ backend/frontend/public/src/assets/images/components/MedicationList.js
@@ -1,1 +1,5 @@
-
+ useEffect(() => {
+    axios.get('/api/medications')
+      .then(response => dispatch(fetchMedicationsSuccess(response.data)))
+      .catch(error => console.error('There was an error fetching the medications!', error));
+  }, [dispatch]);
