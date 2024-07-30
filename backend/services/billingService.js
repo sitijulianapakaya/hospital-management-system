89fed396514d92039e937f8a3992b417b6e3f853
sitijulianapakaya <sitijulianapakaya@gmail.com>
2024-07-30 19:03:31 +0800

    Update billingService.js

--- backend/services/billingService.js
+++ backend/services/billingService.js
@@ -1,1 +1,10 @@
-
+// Mendapatkan daftar billing
+export const getBillings = async () => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/billings`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching billings:', error);
+    throw error;
+  }
+};
