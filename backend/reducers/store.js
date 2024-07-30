f9abad4222a078ec8f4101d5aad1d81a6dcf9b13
sitijulianapakaya <sitijulianapakaya@gmail.com>
2024-07-30 17:34:15 +0800

    Update store.js

--- backend/reducers/store.js
+++ backend/reducers/store.js
@@ -1,1 +1,8 @@
-
+// Menggabungkan semua reducer
+const rootReducer = combineReducers({
+  patients: patientReducer,
+  medications: medicationReducer,
+  doctors: doctorReducer,
+  appointments: appointmentReducer,
+  bills: billingReducer,
+});
