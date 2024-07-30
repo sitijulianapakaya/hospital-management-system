e466c897a273c965b4d2664ac83be9c40bff1918
sitijulianapakaya <sitijulianapakaya@gmail.com>
2024-07-30 19:33:34 +0800

    Update BillingList.js

--- backend/frontend/public/src/assets/images/components/BillingList.js
+++ backend/frontend/public/src/assets/images/components/BillingList.js
@@ -1,1 +1,9 @@
+const BillingList = () => {
+  const dispatch = useDispatch();
+  const bills = useSelector((state) => state.bills.bills);
 
+  const [newBill, setNewBill] = useState({
+    amount: '',
+    date: '',
+    patientName: '',
+  });
