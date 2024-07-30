1e795c60489f2da80ecb0a50becd1b87608113c8
sitijulianapakaya <sitijulianapakaya@gmail.com>
2024-07-30 19:38:10 +0800

    Update PatientList.js

--- backend/frontend/public/src/assets/images/components/PatientList.js
+++ backend/frontend/public/src/assets/images/components/PatientList.js
@@ -1,1 +1,14 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import axios from 'axios';
+import { fetchPatientsSuccess } from './actions/patientActions';
 
+const PatientList = () => {
+  const dispatch = useDispatch();
+  const patients = useSelector((state) => state.patients.patients);
+
+  useEffect(() => {
+    axios.get('/api/patients')
+      .then(response => dispatch(fetchPatientsSuccess(response.data)))
+      .catch(error => console.error('There was an error fetching the patients!', error));
+  }, [dispatch]);
