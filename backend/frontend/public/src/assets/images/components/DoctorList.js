1d3f341c52513567889a1f2dda112421b7fd0129
sitijulianapakaya <sitijulianapakaya@gmail.com>
2024-07-30 19:35:31 +0800

    Update DoctorList.js

--- backend/frontend/public/src/assets/images/components/DoctorList.js
+++ backend/frontend/public/src/assets/images/components/DoctorList.js
@@ -1,1 +1,32 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import axios from 'axios';
+import { fetchDoctorsSuccess } from './actions/doctorActions';
+
+const DoctorList = () => {
+  const dispatch = useDispatch();
+  const doctors = useSelector((state) => state.doctors.doctors);
+
+  useEffect(() => {
+    axios.get('/api/doctors')
+      .then(response => dispatch(fetchDoctorsSuccess(response.data)))
+      .catch(error => console.error('There was an error fetching the doctors!', error));
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h1>Doctor List</h1>
+      <ul>
+        {doctors.map(doctor => (
+          <li key={doctor.id}>
+            {doctor.name} - {doctor.specialty}
+            <a href={`/doctors/${doctor.id}`}>Details</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default DoctorList;
 
