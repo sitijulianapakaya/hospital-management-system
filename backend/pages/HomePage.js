04c2acb65c9487c31f0fb95356faa36f386e415a
sitijulianapakaya <sitijulianapakaya@gmail.com>
2024-07-30 17:14:00 +0800

    Update HomePage.js

--- backend/pages/HomePage.js
+++ backend/pages/HomePage.js
@@ -1,1 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Healthcare Management System</h1>
+      <p>Use the navigation links below to access different sections of the system:</p>
+      <ul>
+        <li><Link to="/appointments">Appointments</Link></li>
+        <li><Link to="/billing">Billing</Link></li>
+        <li><Link to="/doctors">Doctors</Link></li>
+      </ul>
+    </div>
+  );
+};
+
+export default HomePage;
 
