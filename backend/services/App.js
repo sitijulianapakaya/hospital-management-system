0e1de164c8ac7ba7205b770f3786c1f0a0e3929d
sitijulianapakaya <sitijulianapakaya@gmail.com>
2024-07-30 18:56:20 +0800

    Update App.js

--- backend/services/App.js
+++ backend/services/App.js
@@ -1,1 +1,10 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './HomePage';
+import AppointmentPage from './AppointmentPage';
+import DoctorPage from './DoctorPage';
+import PatientPage from './PatientPage';
+import MedicationPage from './MedicationPage';
+import BillingPage from './BillingPage';
+import NotFoundPage from './NotFoundPage'; // Halaman 404 jika route tidak ditemukan
 
