338b3d0891345e159a08f5a3201efcb0b61761b4
sitijulianapakaya <sitijulianapakaya@gmail.com>
2024-07-30 19:09:23 +0800

    Update routes.js

--- backend/services/routes.js
+++ backend/services/routes.js
@@ -1,1 +1,13 @@
-
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/appointments" element={<AppointmentPage />} />
+      <Route path="/doctors" element={<DoctorPage />} />
+      <Route path="/patients" element={<PatientPage />} />
+      <Route path="/medications" element={<MedicationPage />} />
+      <Route path="/billing" element={<BillingPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
+};
