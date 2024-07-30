96cab12bdbdbf92ba0472fa23a90a78460edc82b
sitijulianapakaya <sitijulianapakaya@gmail.com>
2024-07-30 17:29:44 +0800

    Update billingReducer.js

--- backend/reducers/billingReducer.js
+++ backend/reducers/billingReducer.js
@@ -1,1 +1,14 @@
+// store.js
+import { createStore, combineReducers } from 'redux';
+import billingReducer from './billingReducer';
+import appointmentReducer from './appointmentReducer'; // jika Anda juga menggunakan appointmentReducer
+
+const rootReducer = combineReducers({
+  bills: billingReducer,
+  // tambahkan reducer lainnya jika perlu
+});
+
+const store = createStore(rootReducer);
+
+export default store;
 
