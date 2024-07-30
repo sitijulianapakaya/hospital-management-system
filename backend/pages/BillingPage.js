fecd8e4784e819c6648b715e9482f62fe5c183e3
sitijulianapakaya <sitijulianapakaya@gmail.com>
2024-07-30 17:11:52 +0800

    Update BillingPage.js

--- backend/pages/BillingPage.js
+++ backend/pages/BillingPage.js
@@ -1,1 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
+const BillingPage = () => {
+  const [bills, setBills] = useState([]);
+  const [newBill, setNewBill] = useState({ date: '', amount: '', description: '' });
+
+  useEffect(() => {
+    // Fetch bills from API when component mounts
+    axios.get('/api/bills')
+      .then(response => {
+        setBills(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the bills!', error);
+      });
+  }, []);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewBill({
+      ...newBill,
+      [name]: value
+    });
+  };
