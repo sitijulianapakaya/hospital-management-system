b6e8fa9c2fd572d47aae4c39305afc3ba46766da
sitijulianapakaya <sitijulianapakaya@gmail.com>
2024-07-30 17:32:43 +0800

    Update patientReducer.js

--- backend/reducers/patientReducer.js
+++ backend/reducers/patientReducer.js
@@ -1,1 +1,12 @@
+// actionTypes.js
+export const FETCH_PATIENTS_SUCCESS = 'FETCH_PATIENTS_SUCCESS';
+export const ADD_PATIENT_SUCCESS = 'ADD_PATIENT_SUCCESS';
+export const DELETE_PATIENT_SUCCESS = 'DELETE_PATIENT_SUCCESS';
+
+// patientReducer.js
+import { FETCH_PATIENTS_SUCCESS, ADD_PATIENT_SUCCESS, DELETE_PATIENT_SUCCESS } from './actionTypes';
+
+const initialState = {
+  patients: [],
+};
 
