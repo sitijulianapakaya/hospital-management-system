b70825830a7073ae836271628eac633251a0038f
sitijulianapakaya <sitijulianapakaya@gmail.com>
2024-07-30 19:12:39 +0800

    Update billingActions.js

--- backend/store/actions/billingActions.js
+++ backend/store/actions/billingActions.js
@@ -1,1 +1,15 @@
-
+export const addBilling = (billing) => async (dispatch) => {
+  try {
+    const response = await fetch('/api/billings', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(billing)
+    });
+    const data = await response.json();
+    dispatch(addBillingSuccess(data));
+  } catch (error) {
+    dispatch(addBillingFailure(error));
+  }
+};
